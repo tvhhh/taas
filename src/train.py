dc71b531f538bd79a33dc2fbbf484a4336d9b61e
tvhhh <tvhung2112@gmail.com>
2022-03-02 17:43:49 +0700

    Update abstractive training scripts

--- src/train.py
+++ src/train.py
@@ -1,13 +1,6 @@
 import argparse
 
-from datasets import load_dataset, load_from_disk
-
-from transformers.trainer import Trainer
-from transformers.training_args import TrainingArguments
-from transformers.models.pegasus.tokenization_pegasus import PegasusTokenizer
-
 from train_abstractive import train_abs
-from train_extractive import train_ext
 
 
 def str2bool(v):
@@ -19,49 +12,11 @@ def str2bool(v):
         raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
-def train(args, model, data_collator, train_set, eval_set, tokenizer, compute_metrics=None):
-    training_args = TrainingArguments(
-        output_dir=args.output_dir,
-        overwrite_output_dir=(not args.from_checkpoint),
-        evaluation_strategy=args.eval_strategy,
-        per_device_train_batch_size=args.batch_size,
-        per_device_eval_batch_size=args.batch_size,
-        eval_accumulation_steps=args.eval_accumulation_steps,
-        learning_rate=args.lr,
-        weight_decay=args.weight_decay,
-        adam_beta1=args.beta1,
-        adam_beta2=args.beta2,
-        adam_epsilon=args.epsilon,
-        num_train_epochs=args.train_epochs,
-        max_steps=args.train_steps,
-        warmup_ratio=args.warmup_ratio,
-        logging_dir=args.logging_dir,
-        logging_strategy=args.logging_strategy,
-        logging_steps=args.logging_steps,
-        save_strategy=args.save_checkpoint_strategy,
-        save_steps=args.save_checkpoint_steps,
-        eval_steps=args.eval_steps,
-        load_best_model_at_end=args.load_best_at_end,
-        metric_for_best_model=args.metric_load_best,
-        greater_is_better=args.greater_better,
-    )
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=train_set,
-        eval_dataset=eval_set,
-        tokenizer=tokenizer,
-        compute_metrics=compute_metrics,
-    )
-    trainer.train(resume_from_checkpoint=args.from_checkpoint)
-
-
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-task", type=str, choices=["ext", "abs", "test"], default="abs")
+    parser.add_argument("-task", type=str, choices=["ntm", "abs", "test"], default="abs")
 
     parser.add_argument("-load_data_from_disk", type=str2bool, nargs="?", default=False)
     parser.add_argument("-dataset_path", type=str, default="cnn_dailymail")
@@ -69,23 +24,13 @@ if __name__ == "__main__":
     parser.add_argument("-data_input_name", type=str, default="article")
     parser.add_argument("-data_label_name", type=str, default="highlights")
 
-    parser.add_argument("-pretrained_model_path", type=str, default=None)
-    parser.add_argument("-pretrained_pegasus_path", type=str, default="google/pegasus-large")
-    parser.add_argument("-distill_pegasus", type=str2bool, default=False)
-    parser.add_argument("-copied_encoder_layers", type=int, default=16)
-    parser.add_argument("-copied_decoder_layers", type=int, default=4)
     parser.add_argument("-pretrained_tokenizer_path", type=str, default=None)
-    parser.add_argument("-add_special_tokens", type=str2bool, nargs="?", default=True)
+    parser.add_argument("-pretrained_model_path", type=str, default=None)
+    parser.add_argument("-pretrained_pegasus_large_path", type=str, default=None)
+    parser.add_argument("-shrink_pegasus_large", type=str2bool, default=False)
 
     parser.add_argument("-max_input_length", type=int, default=1024)
     parser.add_argument("-max_label_length", type=int, default=128)
-    parser.add_argument("-max_sentence_length", type=int, default=128)
-    parser.add_argument("-ext_summary_size", type=int, default=3)
-
-    parser.add_argument("-stacked_encoder_type", type=str, choices=["classifier", "transformer"], default="transformer")
-    parser.add_argument("-stacked_layers", type=int, default=3)
-    parser.add_argument("-stacked_ffn_dim", type=int, default=4096)
-    parser.add_argument("-stacked_attention_heads", type=int, default=16)
     
     parser.add_argument("-eval_strategy", type=str, choices=["no", "epoch", "steps"], default="epoch")
     parser.add_argument("-eval_steps", type=int, default=5)
@@ -114,24 +59,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    dataset = None
-    if args.load_data_from_disk:
-        dataset = load_from_disk(args.dataset_path)
-    else:
-        dataset = load_dataset(args.dataset_path, args.dataset_name)
-
-    tokenizer = PegasusTokenizer.from_pretrained(args.pretrained_tokenizer_path)
-    if args.add_special_tokens:
-        tokenizer.add_special_tokens({
-            "cls_token": "<cls>",
-            "sep_token": "<sep>",
-        })
+    if args.task == "abs":
+        train_abs(args)
     
-    if args.task == "ext":
-        train_ext(args, tokenizer, dataset, train)
-
-    elif args.task == "abs":
-        train_abs(args, tokenizer, dataset, train)
+    elif args.task == "ntm":
+        pass
 
     elif args.task == "test":
         pass
--- src/train_abstractive.py
+++ src/train_abstractive.py
@@ -1,18 +1,15 @@
 import numpy as np
 import nltk
 
-from datasets import load_metric
+from datasets import load_dataset, load_from_disk, load_metric
 
 from transformers.data.data_collator import DataCollatorForSeq2Seq
+from transformers.models.pegasus.tokenization_pegasus import PegasusTokenizer
+from transformers.trainer import Trainer
+from transformers.training_args import TrainingArguments
 
 from models.configuration_sus import SusConfig
 from models.modeling_sus import SusForConditionalGeneration
-
-
-SUS_INPUT_PREFIX = "sus-formatted"
-
-
-rouge = load_metric("rouge")
 
 
 def _prepare_data(
@@ -22,30 +19,28 @@ def _prepare_data(
     label_name,
     max_input_length=1024,
     max_target_length=128,
-):
-    required_attrs = [f"{SUS_INPUT_PREFIX} {input_name}"]
-    for attr in required_attrs:
-        if attr not in dataset.features:
-            raise ValueError(f"Column {attr} is required in dataset.")
-    
+):    
     def _process_data(examples):
         model_inputs = tokenizer(
-            examples[f"{SUS_INPUT_PREFIX} {input_name}"],
+            examples[input_name],
             max_length=max_input_length,
             truncation=True
         )
+
         with tokenizer.as_target_tokenizer():
             labels = tokenizer(
                 examples[label_name],
                 max_length=max_target_length,
-                truncation=True
+                truncation=True,
             )
         model_inputs["labels"] = labels["input_ids"]
+        
         return model_inputs
 
     return dataset.map(_process_data, batched=True)
 
 
+rouge = load_metric("rouge")
 def _compute_metrics(p, tokenizer):
     (logits, _), labels = p
     predictions = np.argmax(logits, axis=-1)
@@ -63,7 +58,19 @@ def _compute_metrics(p, tokenizer):
     return results
 
 
-def train_abs(args, tokenizer, dataset, train):
+def train_abs(args):
+
+    # Load preprocessed dataset
+    dataset = None
+    if args.load_data_from_disk:
+        dataset = load_from_disk(args.dataset_path)
+    else:
+        dataset = load_dataset(args.dataset_path, args.dataset_name)
+
+    # Load pretrained tokenizer for PEGASUS
+    tokenizer = PegasusTokenizer.from_pretrained(args.pretrained_tokenizer_path)
+
+    # Load or initialize model
     sus = None
     if args.pretrained_model_path is not None:
         sus = SusForConditionalGeneration.from_pretrained(args.pretrained_model_path)
@@ -72,46 +79,66 @@ def train_abs(args, tokenizer, dataset, train):
             cls_token_id=tokenizer.cls_token_id,
             sep_token_id=tokenizer.sep_token_id,
         )
-
-        copied_encoder_layers = None
-        copied_decoder_layers = None
-        if args.distill_pegasus:
-            copied_encoder_layers = args.copied_encoder_layers
-            copied_decoder_layers = args.copied_decoder_layers
         
         sus = SusForConditionalGeneration(
             config,
-            pretrained_pegasus_path=args.pretrained_pegasus_path,
-            shrink_pegasus_large=args.distill_pegasus,
-            copied_encoder_layers=copied_encoder_layers,
-            copied_decoder_layers=copied_decoder_layers,
+            pretrained_pegasus_path=args.pretrained_pegasus_large_path,
+            shrink_pegasus_large=args.shrink_pegasus_large,
         )
-        
-        if args.add_special_tokens:
-            sus.resize_token_embeddings(len(tokenizer))
     
+    # Use data collator for padding
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer,model=sus)
     
+    # Prepare data for abstractive summarization training
     train_set, eval_set = (
         _prepare_data(
             dataset[s],
             tokenizer,
             args.data_input_name,
             args.data_label_name,
-            args.max_sentence_length)
-        for s in ["train", "validation"]
+            args.max_input_length,
+            args.max_target_length)
+        for s in ("train", "validation")
     )
 
+    # Use compute_metrics for validation during training
     compute_metrics = None
     if args.compute_metrics:
         compute_metrics = lambda p: _compute_metrics(p, tokenizer)
 
-    train(
-        args=args,
+    # Use HuggingFace Trainer API
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        overwrite_output_dir=(not args.from_checkpoint),
+        evaluation_strategy=args.eval_strategy,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        eval_accumulation_steps=args.eval_accumulation_steps,
+        learning_rate=args.lr,
+        weight_decay=args.weight_decay,
+        adam_beta1=args.beta1,
+        adam_beta2=args.beta2,
+        adam_epsilon=args.epsilon,
+        num_train_epochs=args.train_epochs,
+        max_steps=args.train_steps,
+        warmup_ratio=args.warmup_ratio,
+        logging_dir=args.logging_dir,
+        logging_strategy=args.logging_strategy,
+        logging_steps=args.logging_steps,
+        save_strategy=args.save_checkpoint_strategy,
+        save_steps=args.save_checkpoint_steps,
+        eval_steps=args.eval_steps,
+        load_best_model_at_end=args.load_best_at_end,
+        metric_for_best_model=args.metric_load_best,
+        greater_is_better=args.greater_better,
+    )
+    trainer = Trainer(
         model=sus,
+        args=training_args,
         data_collator=data_collator,
-        train_set=train_set,
-        eval_set=eval_set,
+        train_dataset=train_set,
+        eval_dataset=eval_set,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics,
     )
+    trainer.train(resume_from_checkpoint=args.from_checkpoint)
